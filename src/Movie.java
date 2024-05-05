e58ebc6f226d1a6fbbf88e34c18f3d50ec78133d
Henrique M. N. <henriquemancininevado@gmail.com>
2024-05-05 13:03:05 -0300

    Criação da classe Filme

---
+++ src/Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+
+    // Atributos da classe Filme
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+
+}
