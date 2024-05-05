7d29067ea2ed85696e55ca415c2c52f07a6bdaad
Henrique M. N. <henriquemancininevado@gmail.com>
2024-05-05 13:29:51 -0300

    Criação de métodos e métodos void/com retorno

--- src/Movie.java
+++ src/Movie.java
@@ -4,8 +4,22 @@ public class Movie {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double retornaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 
 }
